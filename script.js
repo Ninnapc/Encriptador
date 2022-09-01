ec7505d31438f8fec319f6328caf1e46abde1ff9
Ninnapc <sambarreto03@gmail.com>
2022-09-01 18:27:28 -0500

    Carpeta con ejercicio de encriptador para challenge

---
+++ script.js
@@ -0,0 +1,44 @@
+const inputTexto = document.querySelector(".input-texto")
+const mensaje = document.querySelector(".mensaje")
+
+function btnEncriptar(){
+    const textoEncriptado = encriptar(inputTexto.value)
+    mensaje.value = textoEncriptado;
+    mensaje.style.backgroundImage = "none"
+    inputTexto.value = ""
+}
+function encriptar(stringEncriptado){
+       let matrizCodigo = [["e","enter"], ["i","imes"], ["a","ai"], ["o","ober"], ["u","ufat"]];
+       stringEncriptado = stringEncriptado.toLowerCase();
+
+       for(let i = 0; i < matrizCodigo.length; i++){
+          if(stringEncriptado.includes(matrizCodigo[i][0])){
+            stringEncriptado = stringEncriptado.replaceAll(matrizCodigo[i][0],matrizCodigo[i][1])
+          }
+       }
+       return stringEncriptado;
+}
+
+function btnDesencriptar(){
+    const textoEncriptado = desencriptar(inputTexto.value)
+    mensaje.value = textoEncriptado;
+    mensaje.style.backgroundImage = "none";
+    inputTexto.value = "";
+}
+function desencriptar(stringDesencriptado){
+    let matrizCodigo = [["e","enter"],["i","imes"],["a","ai"],["o","ober"],["u","ufat"]];
+    stringDesencriptado = stringDesencriptado.toLowerCase();
+
+    for(let i = 0; i < matrizCodigo.length; i++){
+       if(stringDesencriptado.includes(matrizCodigo[i][1])){
+         stringDesencriptado = stringDesencriptado.replaceAll(matrizCodigo[i][1],matrizCodigo[i][0])
+       }
+    }
+    return stringDesencriptado;
+}
+
+function copiar(){
+    mensaje.select()
+    navigatior.clipboard.writeText(mensaje.value)
+
+}
